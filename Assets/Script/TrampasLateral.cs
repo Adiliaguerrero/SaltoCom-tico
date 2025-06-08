94e4d2d9562978fee41346c1d655d8518f1dcd13
José Ramírez <suarezjose200314@gmail.com>
2025-06-07 19:14:14 -0600

    Finalizados comentarios sobre clase 'Cuchilla'

--- Assets/Script/TrampasLateral.cs
+++ Assets/Script/TrampasLateral.cs
@@ -12,16 +12,12 @@ public class Cuchilla : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Intentamos obtener el componente PlayerController del objeto que colisionó, para verificar si es el jugador
         PlayerController jugador = collision.gameObject.GetComponent<PlayerController>();
 
-        // Si el objeto que colisionó es el jugador
         if (jugador != null)
         {
-            // Definimos la posición actual de la cuchilla para usar como dirección del daño
             Vector2 direccionDanio = transform.position;
 
-            // Llamamos al método del jugador para que reciba daño, pasando la dirección y la cantidad de daño
             jugador.RecibeDanio(direccionDanio, danio);
         }
     }
