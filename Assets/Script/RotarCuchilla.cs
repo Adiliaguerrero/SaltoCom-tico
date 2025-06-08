f9a6fa1fd2f70e2f94072443673c4df90be6ea54
José Ramírez <suarezjose200314@gmail.com>
2025-06-07 18:11:09 -0600

    Agregando documentación general para 'RotarCuchilla'.

--- Assets/Script/RotarCuchilla.cs
+++ Assets/Script/RotarCuchilla.cs
@@ -1,11 +1,13 @@
-// Importamos el espacio de nombres necesario para trabajar con Unity
 using UnityEngine;
 
-// Esta clase trampa hace que un objeto gire continuamente para hacer daño sobre su eje Z con una velocidad configurable 
+    /// <summary>
+    /// Hace que un objeto gire continuamente sobre su eje Z, simulando una cuchilla giratoria dañina.
+    /// Ideal para trampas en niveles de juego.
+    /// </summary>
 public class RotarCuchilla : MonoBehaviour
 {
     // Velocidad de rotación en grados por segundo (ajustable desde el Inspector)
-    public float velocidadRotacion = 180f;  
+    public float velocidadRotacion = 180f;
 
     // Método llamado una vez por frame
     void Update()
